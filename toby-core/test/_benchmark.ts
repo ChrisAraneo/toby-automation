d754375c80f3ad502c9abe1c51f79708b2e4f35c
ChrisAraneo <chris.araneo@gmail.com>
2022-07-17 00:11:49 +0200

    Temporary benchmark update

--- toby-core/test/_benchmark.ts
+++ toby-core/test/_benchmark.ts
@@ -1,78 +1,41 @@
-import axios from "axios";
 import bench from "nanobench";
 import robot from "robotjs";
-import { findImage } from "../src/find-image";
+import { findImages } from "../src/find-image";
+import { Image } from "../src/read-image";
 
 function main() {
   captureScreenBenchmark();
-  fetchSimpleResponseBenchmark();
   fetchImageSearchBenchmark();
 }
 
+const images: Image[] = [
+  {
+    path: "(none)",
+    width: 1,
+    height: 1,
+    pixels: [
+      {
+        red: 230,
+        green: 74,
+        blue: 25,
+      },
+    ],
+  },
+];
+
 function fetchImageSearchBenchmark() {
-  bench("Find Image (Python)", function (b: any) {
+  bench("Find Image", function (b: any) {
     b.start();
-    findImage("example.png")
-      .then(() => {
-        b.end();
-      })
-      .catch(() => b.end());
-  });
-}
-
-type Request = {
-  images: {
-    width: number;
-    height: number;
-    pixels: {
-      red: number;
-      green: number;
-      blue: number;
-    }[];
-  }[];
-};
-
-const body: Request = {
-  images: [
-    {
-      width: 1,
-      height: 1,
-      pixels: [
-        {
-          red: 230,
-          green: 74,
-          blue: 25,
-        },
-      ],
-    },
-  ],
-};
-
-function fetchSimpleResponseBenchmark() {
-  bench("Find Image (Go)", function (b: any) {
-    b.start();
-    test()
+    findImages(images)
       .then(() => b.end())
       .catch(() => b.end());
-  });
-}
-
-function test(): Promise<void> {
-  return new Promise<void>(async (resolve, reject) => {
-    try {
-      await axios.post("http://localhost:8080/", { ...body });
-      resolve();
-    } catch (e: any) {
-      reject(e);
-    }
   });
 }
 
 function captureScreenBenchmark() {
   bench("Capture screen", function (b: any) {
     b.start();
-    const screen = robot.screen.capture();
-    console.log(screen.image);
+    robot.screen.capture();
     b.end();
   });
 }
