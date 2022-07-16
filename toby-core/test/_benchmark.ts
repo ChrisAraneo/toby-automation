981ebfe6b43b1ae9e92369eac10c622c8818d29b
Krzysztof Pajk <chris.araneo@gmail.com>
2022-07-16 13:40:11 +0200

    New benchmark

--- toby-core/test/_benchmark.ts
+++ toby-core/test/_benchmark.ts
@@ -1,23 +1,55 @@
 import axios from "axios";
 import bench from "nanobench";
+import robot from "robotjs";
 import { findImage } from "../src/find-image";
 
 function main() {
+  captureScreenBenchmark();
   fetchSimpleResponseBenchmark();
   fetchImageSearchBenchmark();
 }
 
 function fetchImageSearchBenchmark() {
-  bench("Find Image", function (b: any) {
+  bench("Find Image (Python)", function (b: any) {
     b.start();
     findImage("example.png")
-      .then(() => b.end())
+      .then(() => {
+        b.end();
+      })
       .catch(() => b.end());
   });
 }
 
+type Request = {
+  images: {
+    width: number;
+    height: number;
+    pixels: {
+      red: number;
+      green: number;
+      blue: number;
+    }[];
+  }[];
+};
+
+const body: Request = {
+  images: [
+    {
+      width: 1,
+      height: 1,
+      pixels: [
+        {
+          red: 230,
+          green: 74,
+          blue: 25,
+        },
+      ],
+    },
+  ],
+};
+
 function fetchSimpleResponseBenchmark() {
-  bench("Test connection", function (b: any) {
+  bench("Find Image (Go)", function (b: any) {
     b.start();
     test()
       .then(() => b.end())
@@ -28,11 +60,20 @@ function fetchSimpleResponseBenchmark() {
 function test(): Promise<void> {
   return new Promise<void>(async (resolve, reject) => {
     try {
-      await axios.post("http://localhost:5000/hi", {});
+      await axios.post("http://localhost:8080/", { ...body });
       resolve();
     } catch (e: any) {
       reject(e);
     }
+  });
+}
+
+function captureScreenBenchmark() {
+  bench("Capture screen", function (b: any) {
+    b.start();
+    const screen = robot.screen.capture();
+    console.log(screen.image);
+    b.end();
   });
 }
 
