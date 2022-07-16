c667a68df7b2cade3e97728b36b6688bfc0bea12
Krzysztof Pajk <chris.araneo@gmail.com>
2022-07-16 13:38:31 +0200

    Example request to image search v2 service

--- toby-core/src/app.ts
+++ toby-core/src/app.ts
@@ -1,14 +1,43 @@
-import { findImage } from "./find-image";
+import axios from "axios";
 import { moveMouse } from "./move-mouse";
 
 function app() {
   moveMouse(100, 100, 0.001).then(() => {
     console.log("Moved mouse!");
-
-    findImage("example.png")
-      .then((result) => console.log(result))
-      .catch((e) => console.error(e));
   });
 }
 
 app();
+
+type Request = {
+  images: {
+    width: number;
+    height: number;
+    pixels: {
+      red: number;
+      green: number;
+      blue: number;
+    }[];
+  }[];
+};
+
+const body: Request = {
+  images: [
+    {
+      width: 1,
+      height: 1,
+      pixels: [
+        {
+          red: 230,
+          green: 74,
+          blue: 25,
+        },
+      ],
+    },
+  ],
+};
+
+axios
+  .post("http://localhost:8080/", { ...body })
+  .then((r) => console.log(r.data.data))
+  .catch((e) => console.error(e));
