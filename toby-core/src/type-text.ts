9e9f44491b123655b55080281a8639d6319de7be
ChrisAraneo <chris.araneo@gmail.com>
2022-07-18 21:08:05 +0200

    Type text another solution

--- toby-core/src/type-text.ts
+++ toby-core/src/type-text.ts
@@ -5,14 +5,9 @@ import robot from "robotjs";
  * @param {string} text string to type
  */
 export function typeText(text: string): Promise<void> {
-  const letters = text.split("");
-
   return new Promise<void>((resolve, reject) => {
     try {
-      letters.forEach((letter) => {
-        const charactersPerMinute = Math.floor(220 + Math.random() * 80);
-        robot.typeStringDelayed(letter, charactersPerMinute);
-      });
+      robot.typeString(text);
       resolve();
     } catch (e: any) {
       reject(e);
