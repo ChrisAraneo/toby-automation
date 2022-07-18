6a0f60dbdb59f42c3b9560ec0ef98b7b3d8f2e6b
ChrisAraneo <chris.araneo@gmail.com>
2022-07-18 17:06:54 +0200

    Process image search service response

--- toby-core/src/find-images.ts
+++ toby-core/src/find-images.ts
@@ -20,7 +20,19 @@ export function findImages(images: Image[]): Promise<ImageLocationOnScreen[]> {
   return new Promise<ImageLocationOnScreen[]>((resolve, reject) => {
     axios
       .post("http://localhost:8080/", { images })
-      .then((response: ImageSearchResponse) => resolve(response.data.data))
+      .then((response: ImageSearchResponse) => {
+        const results = [];
+
+        response.data.data.forEach((item) => {
+          const result = {};
+          Object.getOwnPropertyNames(item).forEach((key) => {
+            result[key.toLocaleLowerCase()] = item[key];
+          });
+          results.push(result);
+        });
+
+        resolve(results);
+      })
       .catch((e) => reject(e));
   });
 }
