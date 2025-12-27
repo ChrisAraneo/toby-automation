9b70fabbc7a4682b7d71d22de4eb57c0a5bd8f44
Krzysztof Pająk <chris.araneo@gmail.com>
2025-12-27 20:46:21 +0100

    fix: update path, load autoit on app start

--- toby-core/src/app.ts
+++ toby-core/src/app.ts
@@ -1,9 +1,12 @@
+import { autoit } from "@ahmic/autoit-js";
 import { findImages } from "./find-images";
 import { moveMouse } from "./move-mouse";
 import { readImage } from "./read-image";
 
-function app() {
-  readImage("../toby-core/assets/example.png").then((image) => {
+async function app() {
+  autoit.load();
+
+  readImage("../assets/example.png").then((image) => {
     moveMouse(100, 100, { speed: 1 }).then(() => {
       findImages([image])
         .then((r) => console.log(r))
